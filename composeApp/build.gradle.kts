546d511961f2a504aba8ade966391aa2331b78d4
davberamo <dberlinches2003@gmail.com>
2026-02-18 16:11:11 +0100

    Configuración de la navegación y estructura base con Scaffold

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,6 +41,7 @@ kotlin {
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.navigation.compose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
         }
         commonTest.dependencies {
--- composeApp/src/commonMain/kotlin/org/example/examen/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/examen/App.kt
@@ -1,49 +1,71 @@
 package org.example.examen
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import org.jetbrains.compose.resources.painterResource
 
 import enunciadoexamen.composeapp.generated.resources.Res
-import enunciadoexamen.composeapp.generated.resources.compose_multiplatform
+import enunciadoexamen.composeapp.generated.resources.logoE
+import org.example.examen.ui.screen.Pantallas
+import org.example.examen.ui.theme.BlueDeep
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+fun AppEmpleo() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text ("App Empleo") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = BlueDeep,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White
+                ),
+                navigationIcon = {
+                    Image(
+                        painter = painterResource(Res.drawable.logoE),
+                        contentDescription = "Logo Empleo",
+                        modifier = Modifier.size(40.dp).padding(8.dp)
+                    )
                 }
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+
             }
+        }
+    ) { innerPadding ->
+        // Contenedor de navegación (NavHost)
+        NavHost(
+            navController = navController,
+            startDestination = Pantallas.Bienvenida.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Pantallas.Bienvenida.name) { 
+                // ScreenBienvenida(navController) 
+            }
+            composable(Pantallas.Inscripcion.name) { /* Punto 2 */ }
+            composable(Pantallas.Solicitudes.name) { /* Punto 3 */ }
+            composable(Pantallas.Ayuda.name) { /* Punto 5 */ }
         }
     }
 }
