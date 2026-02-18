60a537581e3b8e38b4251d9b5da254953303253b
davberamo <dberlinches2003@gmail.com>
2026-02-18 16:12:37 +0100

    Creación de la pantalla de bienvenida

---
+++ composeApp/src/commonMain/kotlin/org/example/examen/ui/screen/ScreenBienvenida.kt
@@ -0,0 +1,9 @@
+package org.example.examen.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ScreenBienvenida(navController: NavHostController) {
+
+}
