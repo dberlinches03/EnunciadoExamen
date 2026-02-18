edb43609d3bb0727f879fc82a3a9c76a2e5f87cb
davberamo <dberlinches2003@gmail.com>
2026-02-18 15:42:38 +0100

    Creando los colores para el enunciado

---
+++ composeApp/src/commonMain/kotlin/org/example/examen/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package org.example.examen.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlueLight = Color(0xFF82A1D1) // Azul Claro
+val BlueMedium = Color(0xFF5572B2) // Azul
+val BlueDark = Color(0xFF344C92) // Azul Oscuro
+val BlueDeep = Color(0xFF1C2751)
+val GreenGray = Color(0xFF8A9586)
+val BlueSky = Color(0xFF4898BB) // Azul Cielo
+val BlueCyan = Color(0xFF48AABB)
+val Teal = Color(0xFF308885)
+val Indigo = Color(0xFF364889)
