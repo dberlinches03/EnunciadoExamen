2be660c26ede3392b7368999293fb95ffbd0e3a7
davberamo <dberlinches2003@gmail.com>
2026-02-18 16:32:34 +0100

    Creación del modelo Persona y actualización del punto de entrada a AppEmpleo

---
+++ composeApp/src/commonMain/kotlin/org/example/examen/models/Persona.kt
@@ -0,0 +1,12 @@
+package org.example.examen.models
+
+data class Persona(
+    val nombre: String,
+    val dni: String,
+    val edad: Int,
+    val sexo: String,
+    val direccion: String,
+    val email: String,
+    val telefono: String,
+    val habilidades: String
+)
